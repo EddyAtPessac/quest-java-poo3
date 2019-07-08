cf29481ae7fe4b4c60989840555bbabb76716667
Eddy LAFOND <edbricole@gmail.com>
2019-07-08 11:37:01 +0200

    Commit initial des 6 classes.

--- Eagle.java
+++ Eagle.java
@@ -1,5 +1,6 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
+    public static final double MAX_LAND_ALT = 5; // Maximum altitude for landing
     private boolean flying;
     private int altitude;
 
@@ -15,6 +16,58 @@ public class Eagle extends Bird {
 
     public boolean isFlying() {
         return flying;
+    }
+
+    public void takeOff() {
+        if (!isFlying()) {
+            System.out.println(getName() + " taking off.");
+            this.flying=true;
+        }
+        else {
+            System.out.println(getName() + " already flying.");
+        }
+    }
+    public void ascend(int meters) {
+        if (isFlying()) {
+            this.altitude = Math.min(this.altitude + meters, 1500);
+            System.out.println(getName() +" fly upware. Altitude:" + getAltitude() + "");
+        }
+        else {
+            System.out.println(getName() + " not flying.");
+        }
+    }
+
+    public void descend(int meters) {
+        if (isFlying()) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.println(getName() +" fly downware. Altitude:" + getAltitude() + "");
+        }
+        else {
+            System.out.println(getName() + " not flying.");
+        }
+    }
+
+    public void land (){
+        if (isFlying()) {
+            if (getAltitude()< MAX_LAND_ALT) {
+                System.out.println(getName() + " Landing.");
+                this.altitude=0;
+                this.flying=false;
+            }
+        }
+        else {
+            System.out.println(getName() + " not flying.");
+        }
+
+    }
+
+    public void glide() {
+        if (isFlying()) {
+            System.out.println(getName() +" glide. Altitude:" + getAltitude() + "");
+        }
+        else {
+            System.out.println(getName() + " not flying.");
+        }
     }
 
     @Override
